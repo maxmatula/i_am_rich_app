bfa905cb26b09c9b21c380979d35ce7f4770677d
Maksymilian Matuła <maksymilian.matula@gmail.com>
2020-01-19 10:45:23 +0100

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(
+      MaterialApp(
+        home: Scaffold(
+          backgroundColor: Colors.blueGrey[400],
+          appBar: AppBar(
+            title: Text('I am rich'),
+            backgroundColor: Colors.blueGrey[900],
+          ),
+          body: Center(
+            child: Image(
+              image: AssetImage('images/diamond.png'),
+            ),
+          ),
+        ),
+      ),
+    );
